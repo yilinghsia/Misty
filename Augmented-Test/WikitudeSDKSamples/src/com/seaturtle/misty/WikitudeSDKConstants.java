4b242a591ef5bade950bfc263387e83c99621194
ruudlok <r.lok91@gmail.com>
2014-10-13 15:15:10 +0200

    Changes: Package name changes Project name changes Boatview added start importing geodata

---
+++ Augmented-Test/WikitudeSDKSamples/src/com/seaturtle/misty/WikitudeSDKConstants.java
@@ -0,0 +1,11 @@
+package com.seaturtle.misty;
+
+public class WikitudeSDKConstants {
+	
+
+	/**
+	 * note that this key is only valid for this sample app, if you need one for your own application please visit www.wikitude.com (SDK Store)
+	 */
+	protected static final String WIKITUDE_SDK_KEY = "hnmY8+8QXrv1Nq0e27V7dYE0zYQ88mJzC5YEYibGHDjKsFjYR5Q7UVvaJcq8WEe9D1gV8YkyA59jWtnNN2UmB825gXise/Pmk0j/FUkQ34R9baB18RC8MpiqK7pKmIEnoPCi511kWY1U7IeNBzQT3gBY680m8XIAtCYch1L1DJRTYWx0ZWRfXwXlnJBp5lrXx3aKPzKfuhW0tq3uhYGr++wOa3TnBV5YCiwlhG1+QssIT3CC0o1vogmqTnyKQ4icAr1SZsXk44Ab5LkwFw2tNlX0KI5zGJD0lxJD77UOa2AqJHKOgllKUcAyZeCwqOpXjer3rv3LI6kUgUJgjK2XK2JpjARoo7v1GV6Yv/zhmNmN1+sFSVubt4yKlsNh3geMbcaTQhuHJMF9ZzmngcpuJcZKW2CwlrXaLiTeCBzx16Rx4ISkIn4TOBEGt0iIOKtA5lMfCHjGydnrggdpwbVKZmj5VNxPqI5zPwiG8ozQs7hkiE7HHBr4tsZhzCKtPBFcDpX4AJJplv1rWf+7HIpumIwNP/WgUKc9yXTcvQ3Zev7bKkIT1vUffqptKT2qCs//UIlydJ3DN0rmWpnWWKPeeP2RNJfVRDJI/qJREZi9JQuoa42cCSp9I8VHa7gs8SbT4JRAhk6a6fSPgDEKA2Z3hQ==";
+
+}
