6877e8fadec32821e882375f0b8254180939cd13
ruudlok <r.lok91@gmail.com>
2014-10-27 15:51:34 +0100

    Commite 2 aanpassing UI

--- Augmented-Test/WikitudeSDKSamples/assets/samples/1_Show$a$view_1_Show$BoatFinder/js/marker.js
+++ Augmented-Test/WikitudeSDKSamples/assets/samples/1_Show$a$view_1_Show$BoatFinder/js/marker.js
@@ -16,18 +16,11 @@ function Marker(poiData) {
         /*
             To react on user interaction, an onClick property can be set for each AR.Drawable. The property is a function which will be called each time the user taps on the drawable. The function called on each tap is returned from the following helper function defined in marker.js. The function returns a function which checks the selected state with the help of the variable isSelected and executes the appropriate function. The clicked marker is passed as an argument.
         */
-        onClick: Marker.prototype.getOnClickTrigger(this)
-    });
-
-    // create an AR.ImageDrawable for the marker in selected state
-    this.markerDrawable_selected = new AR.ImageDrawable(World.markerDrawable_selected, 2.5, {
-        zOrder: 0,
-        opacity: 0.0,
-        onClick: null
+        
     });
 
     // create an AR.Label for the marker's title 
-    this.titleLabel = new AR.Label(poiData.title.trunc(10), 1, {
+    this.titleLabel = new AR.Label(poiData.title.trunc(45), 1, {
         zOrder: 1,
         offsetY: 0.55,
         style: {
@@ -37,7 +30,7 @@ function Marker(poiData) {
     });
 
     // create an AR.Label for the marker's description
-    this.descriptionLabel = new AR.Label(poiData.description.trunc(15), 0.8, {
+    this.descriptionLabel = new AR.Label(poiData.description.trunc(100), 0.8, {
         zOrder: 1,
         offsetY: -0.55,
         style: {
@@ -48,50 +41,12 @@ function Marker(poiData) {
     // create the AR.GeoObject with the drawable objects
     this.markerObject = new AR.GeoObject(markerLocation, {
         drawables: {
-            cam: [this.markerDrawable_idle, this.markerDrawable_selected, this.titleLabel, this.descriptionLabel]
+            cam: [this.markerDrawable_idle, this.titleLabel, this.descriptionLabel]
         }
     });
 
     return this;
 }
-
-Marker.prototype.getOnClickTrigger = function(marker) {
-
-    /*
-        The setSelected and setDeselected functions are prototype Marker functions.
-
-        Both functions perform the same steps but inverted, hence only one function (setSelected) is covered in detail. Three steps are necessary to select the marker. First the state will be set appropriately. Second the background drawable will be enabled and the standard background disabled. This is done by setting the opacity property to 1.0 for the visible state and to 0.0 for an invisible state. Third the onClick function is set only for the background drawable of the selected marker.
-    */
-
-    return function() {
-
-        if (marker.isSelected) {
-
-            Marker.prototype.setDeselected(marker);
-
-        } else {
-            Marker.prototype.setSelected(marker);
-            try {
-                World.onMarkerSelected(marker);
-            } catch (err) {
-                alert(err);
-            }
-
-        }
-
-        return true;
-    };
-};
-
-Marker.prototype.setSelected = function(marker) {
-
-    marker.isSelected = true;
-
-    marker.markerDrawable_idle.opacity = 0.0;
-    marker.markerDrawable_selected.opacity = 1.0;
-    marker.markerDrawable_idle.onClick = null;
-    marker.markerDrawable_selected.onClick = Marker.prototype.getOnClickTrigger(marker);
-};
 
 Marker.prototype.setDeselected = function(marker) {
 
--- Augmented-Test/WikitudeSDKSamples/assets/samples/1_Show$a$view_1_Show$BoatFinder/js/multiplepois.js
+++ Augmented-Test/WikitudeSDKSamples/assets/samples/1_Show$a$view_1_Show$BoatFinder/js/multiplepois.js
@@ -5,7 +5,6 @@ var World = {
 
 	// different POI-Marker assets
 	markerDrawable_idle: null,
-	markerDrawable_selected: null,
 
 	// list of AR.GeoObjects that are currently shown in the scene / World
 	markerList: [],
@@ -19,8 +18,7 @@ var World = {
 		World.markerList = [];
 
 		// start loading marker assets
-		World.markerDrawable_idle = new AR.ImageResource("assets/marker_idle.png");
-		World.markerDrawable_selected = new AR.ImageResource("assets/marker_selected.png");
+		World.markerDrawable_idle = new AR.ImageResource("assets/boatinfo.png");
 
 		// loop through POI-information and create an AR.GeoObject (=Marker) per POI
 		for (var currentPlaceNr = 0; currentPlaceNr < poiData.length; currentPlaceNr++) {
@@ -40,22 +38,6 @@ var World = {
 			World.markerList.push(new Marker(singlePoi));
 		}
 
-		World.updateStatusMessage(currentPlaceNr + ' places loaded');
-	},
-
-	// updates status message shon in small "i"-button aligned bottom center
-	updateStatusMessage: function updateStatusMessageFn(message, isWarning) {
-
-		var themeToUse = isWarning ? "e" : "c";
-		var iconToUse = isWarning ? "alert" : "info";
-
-		$("#status-message").html(message);
-		$("#popupInfoButton").buttonMarkup({
-			theme: themeToUse
-		});
-		$("#popupInfoButton").buttonMarkup({
-			icon: iconToUse
-		});
 	},
 
 	// location updates, fired every time you call architectView.setLocation() in native environment
@@ -73,45 +55,28 @@ var World = {
 		}
 	},
 
-	// fired when user pressed maker in cam
-	onMarkerSelected: function onMarkerSelectedFn(marker) {
-
-		// deselect previous marker
-		if (World.currentMarker) {
-			if (World.currentMarker.poiData.id == marker.poiData.id) {
-				return;
-			}
-			World.currentMarker.setDeselected(World.currentMarker);
-		}
-
-		// highlight current one
-		marker.setSelected(marker);
-		World.currentMarker = marker;
-	},
-
-	// screen was clicked but no geo-object was hit
-	onScreenClick: function onScreenClickFn() {
-		if (World.currentMarker) {
-			World.currentMarker.setDeselected(World.currentMarker);
-		}
-	},
-
 	// request POI data
 	requestDataFromLocal: function requestDataFromLocalFn(centerPointLatitude, centerPointLongitude) {
-		var poisToCreate = 20;
-		var poiData = [];
 
-		for (var i = 0; i < poisToCreate; i++) {
-			poiData.push({
-				"id": (i + 1),
-				"longitude": (centerPointLongitude + (Math.random() / 5 - 0.1)),
-				"latitude": (centerPointLatitude + (Math.random() / 5 - 0.1)),
-				"description": ("This is the description of POI#" + (i + 1)),
-				"altitude": "100.0",
-				"name": ("POI#" + (i + 1))
+		var id = 1;
+		var latitude = 51.9176436;
+		var longitude = 4.4823579;
+		var description = "Een lange beschrijving"
+		var altitude = "100.0"
+		var name = "Schipnaam Awesome"	
+		
+		var ShipData = [];
+
+			ShipData.push({
+				"id": id,
+				"longitude": (longitude),
+				"latitude": (latitude),
+				"description": (description),
+				"altitude": altitude,
+				"name": (name)
 			});
-		}
-		World.loadPoisFromJsonData(poiData);
+		
+		World.loadPoisFromJsonData(ShipData);
 	}
 
 };
@@ -120,8 +85,3 @@ var World = {
 	Set a custom function where location changes are forwarded to. There is also a possibility to set AR.context.onLocationChanged to null. In this case the function will not be called anymore and no further location updates will be received. 
 */
 AR.context.onLocationChanged = World.locationChanged;
-
-/*
-	To detect clicks where no drawable was hit set a custom function on AR.context.onScreenClick where the currently selected marker is deselected.
-*/
-AR.context.onScreenClick = World.onScreenClick;
